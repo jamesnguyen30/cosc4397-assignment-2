a9ba5d23bdaa749fe2e9908838750283da67aab3
Khuong Nguyen <nguyenthecoder@gmail.com>
2021-02-25 18:11:36 -0500

    Updated on Feb 25

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,14 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
 
-    public Button forward, backward, left, right, positionButton, rotationButton, scaleUpButton, scaleDownButton;
+    public Slider rotateSlider;
+    public Slider scaleSlider;
+
+    private float newRotation = 0.0f;
+    private float scaleValue = 0.0f;
 
     public GameObject gameObject;
-    private float movingSpeed = 200.0f;
+    // private float movingSpeed = 200.0f;
 
     private bool isPositioning = false;
 
@@ -19,86 +24,36 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         scaleVector = new Vector3(scaleFactor, scaleFactor, scaleFactor);
-        right.onClick.AddListener(RightButtonClicked);
-        left.onClick.AddListener(LeftButtonClicked);
-        forward.onClick.AddListener(ForwardButtonClicked);
-        backward.onClick.AddListener(BackwardButtonClicked);
-        positionButton.onClick.AddListener(PositionButtonClicked);
-        rotationButton.onClick.AddListener(RotationnButtonClicked);
-        scaleUpButton.onClick.AddListener(ScaleUpClicked);
-        scaleDownButton.onClick.AddListener(ScaleDownClicked);
+        scaleSlider.maxValue = 20.0f;
+        scaleSlider.minValue = 1.0f;
 
+        rotateSlider.maxValue = 360f;
+        rotateSlider.minValue = 0f;
     }
     // Update is called once per frame
     void Update()
     {
-    
+        RotationUpdate();
+        ScaleUpdate();
     }
-    
-    void RightButtonClicked() {
-        if (isPositioning == true)
-        {
-    	gameObject.transform.Translate(Vector3.right * movingSpeed * Time.deltaTime);
-    	return;
-        }
-        else
-        {
-    	gameObject.transform.Rotate(Vector3.down * movingSpeed * Time.deltaTime);
-    	return;
-        }
+
+    void RotationUpdate()
+    {
+        gameObject.transform.eulerAngles = new Vector3(
+            gameObject.transform.eulerAngles.x,
+            rotateSlider.value,
+            gameObject.transform.eulerAngles.x
+        );
     }
-    
-    void LeftButtonClicked() {
-        if (isPositioning == true)
-        {
-    	gameObject.transform.Translate(Vector3.left * movingSpeed * Time.deltaTime);
-    	return;
-        }
-        else
-        {
-    	gameObject.transform.Rotate(Vector3.up * movingSpeed * Time.deltaTime);
-    	return;
-        }
+
+    void ScaleUpdate()
+    {
+        gameObject.transform.localScale = new Vector3(
+            scaleSlider.value,            
+            scaleSlider.value,            
+            scaleSlider.value            
+        );
     }
-    void ForwardButtonClicked() {
-        Debug.Log("forward");
-        if (isPositioning == true)
-        {
-    	gameObject.transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);
-    	return;
-        }
-        else
-        {
-    	return;
-        }
-    }
+  
     
-    void BackwardButtonClicked() {
-        Debug.Log("backward");
-        if (isPositioning == true)
-        {
-    	gameObject.transform.Translate(Vector3.back * movingSpeed * Time.deltaTime);
-    	return;
-        }
-        else
-        {
-    	return;
-        }
-    }
-    
-    void PositionButtonClicked() {
-        isPositioning = true;
-    }
-    
-    void RotationnButtonClicked() {
-        isPositioning = false;
-    }
-    
-    void ScaleUpClicked() {
-        gameObject.transform.localScale += scaleVector;
-    }
-    
-    void ScaleDownClicked() {
-        gameObject.transform.localScale -= scaleVector;
-	}
 }
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # COSC 4397 Immersive Tech - Professor Yun - Thursday 5:30pm - 8:30pm
 # Assignment 2
 
-![](readme.gif)
+# Latest updated on Feb 25, 2021
+# The new version has sliders for rotation and slider
 
